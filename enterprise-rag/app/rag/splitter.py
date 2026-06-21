b56402835598d0c80450673169ab3b2d3730b6ae
azizajmalkhan <khanazizajmal@gmail.com>
2026-06-21 22:52:32 +0530

    aknak

--- enterprise-rag/app/rag/splitter.py
+++ enterprise-rag/app/rag/splitter.py
@@ -9,13 +9,42 @@
 #     return splitter.split_text(text)
 
 
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_text_splitters import RecursiveCharacterTextSplitter  
 
+from langchain_experimental.text_splitter import SemanticChunker
+
+# def split_text(text):
+
+#     splitter = RecursiveCharacterTextSplitter(
+#         # chunk_size=500,
+#         # chunk_overlap=50
+#         chunk_size = 100,
+#         chunk_overlap = 0
+#     )
+
+#     return splitter.split_text(text)
+
+
+from langchain_experimental.text_splitter import SemanticChunker
+from app.rag.embedder import embeddings
+ 
 def split_text(text):
 
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50
+    splitter = SemanticChunker(
+        embeddings()
     )
 
-    return splitter.split_text(text)
+    docs = splitter.create_documents([text])
+
+    chunks = [
+        doc.page_content
+        for doc in docs
+    ]
+
+    print("Total Semantic Chunks:", len(chunks))
+
+    # for i, chunk in enumerate(chunks):
+    #     print(f"\n===== Chunk {i+1} =====")
+    #     print(chunk)
+#hello sdsd
+    return chunks
