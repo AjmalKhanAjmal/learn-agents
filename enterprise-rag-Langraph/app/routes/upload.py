11dc57ee5e418790f56faa8e56e4bd880bd76e5d
azizajmalkhan <khanazizajmal@gmail.com>
2026-07-05 22:46:56 +0530

    added langraph folder

--- enterprise-rag-Langraph/app/routes/upload.py
+++ enterprise-rag-Langraph/app/routes/upload.py
@@ -2,10 +2,76 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.post("/upload")
-def upload():
-    return {
-        "status":"sucesss",
-        "message":"API got hit"
-    }
+import os
 
+from fastapi import (
+    APIRouter,
+    UploadFile,
+    File,
+    HTTPException
+)
+
+
+router = APIRouter(
+    prefix="/documents",
+    tags=["Documents"]
+)
+
+
+UPLOAD_DIR = "uploads"
+
+
+@router.post("/upload")
+async def upload_document(
+    file: UploadFile = File(...)
+):
+    # Validate file type
+    if file.content_type != "application/pdf":
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF files are allowed"
+        )
+
+    # Create uploads folder if missing
+    os.makedirs(
+        UPLOAD_DIR,
+        exist_ok=True
+    )
+
+    # Build final file path
+    file_path = os.path.join(
+        UPLOAD_DIR,
+        file.filename
+    )
+
+    try:
+        # Read uploaded file
+        file_content = await file.read()
+
+        # Save file locally
+        with open(
+            file_path,
+            "wb"
+        ) as output_file:
+            output_file.write(
+                file_content
+            )
+
+        return {
+            "status": "success",
+            "message": "PDF uploaded successfully",
+            "file_name": file.filename,
+            "file_path": file_path
+        }
+
+    except Exception as error:
+        raise HTTPException(
+            status_code=500,
+            detail=(
+                "File upload failed: "
+                f"{str(error)}"
+            )
+        )
+
+    finally:
+        await file.close()
